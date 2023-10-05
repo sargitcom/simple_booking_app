13506009e89c97ab266d42ac2d04d9988fb410af
Kamil Pietrzkiewicz <administration@magickpixel.com>
2023-10-05 03:12:09 +0200

    Add orm mapping files

--- src/app/src/Application/Event/Domain/AvailableEventDay.php
+++ src/app/src/Application/Event/Domain/AvailableEventDay.php
@@ -76,52 +76,7 @@ class AvailableEventDay extends AggregateRoot
 
     private function assertValidDate(DateTime $date)
     {
-        $currentDate = new DateTime();
-
-        if ($this->isEventDayYearLessthatCurrentYear($currentDate, $date)) {
-            return false;
-        }
-
-        if ($this->isEventDayYearSameAsCurrentYear($currentDate, $date)) {
-            if ($this->isEventMonthLessThanCurrentMonth($currentDate, $date)) {
-                return false;
-            }
-
-            if ($this->isEventDayMonthSameAsCurrentMonth($currentDate, $date)) {
-                if ($this->isEventDayDayLessThatCurrentDay($currentDate, $date)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        return true;
-    }
-
-    private function isEventDayYearLessthatCurrentYear(DateTime $currentDate, DateTime $eventDayDate) : bool
-    {
-        return $eventDayDate->format('Y') < $currentDate->format('Y');
-    }
-
-    private function isEventDayYearSameAsCurrentYear(DateTime $currentDate, DateTime $eventDayDate) : bool
-    {
-        return $eventDayDate->format('Y') === $currentDate->format('Y');
-    }
-
-    private function isEventMonthLessThanCurrentMonth(DateTime $currentDate, DateTime $eventDayDate) : bool
-    {
-        return $eventDayDate->format('m') < $currentDate->format('m');
-    }
-
-    private function isEventDayMonthSameAsCurrentMonth(DateTime $currentDate, DateTime $eventDayDate) : bool
-    {
-        return $eventDayDate->format('m') === $currentDate->format('m');
-    }
-
-    private function isEventDayDayLessThatCurrentDay(DateTime $currentDate, DateTime $eventDayDate) : bool
-    {
-        return $eventDayDate->format('d') < $currentDate->format('d');
+        return ValidateDate::create()->isDateEqualOrGreaterThanCurrentDate($date);
     }
 
     private function setId($id) : void
